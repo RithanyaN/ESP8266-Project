97d86de6f5d84d4300bb47e77b2de38edd9dd6ff
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2023-12-04 19:35:38 +0530

    Create Station mode.ino

---
+++ WiFi mode of ESP8266/Station mode.ino
@@ -0,0 +1,28 @@
+#include<ESP8266WiFi.h>
+void WiFiConnection();
+void setup()
+{
+  Serial.begin(9600);
+  WiFiConnection();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void WiFiConnection()
+{
+  WiFi.mode(WIFI_STA);  //Connecting ESP8266 via Station mode
+  WiFi.begin("realme C11","rithi:phone");
+  Serial.print("Connecting to WiFi"); 
+  //WiFi.status will return the WL_CONNECTED when it is connected to the WiFi router if not print the folloeing satus
+  while(WiFi.status()!=WL_CONNECTED)  
+  {
+    Serial.println("Trying to connect to WiFi");
+    delay(100);
+  }
+  Serial.print("IP Adress:");
+  Serial.print(WiFi.localIP());
+  Serial.print("MacAddress");
+  Serial.print(WiFi.macAddress());
+}
