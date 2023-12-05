7487aabd8f20f54678eb505dfe2e0e93b827a892
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2023-12-05 19:20:39 +0530

    Update Access point mode.ino

--- WiFi mode of ESP8266/Access point mode.ino
+++ WiFi mode of ESP8266/Access point mode.ino
@@ -1,11 +1,11 @@
 #include<ESP8266WiFi.h>
 #include<DNSServer.h>
 #include<WiFiManager.h>
-WiFiManager wifi;
+WiFiManager wifi;//wifi object of WiFiManager
 void setup()
 {
 Serial.begin(115200);
-wifi.autoConnect("Rithanya's Hotspot Area");
+wifi.autoConnect("Rithanya's Hotspot Area");//Creating the hotspot-area manually if not available and providing the internet to it via Wifi Manager
 Serial.println("connected to Hotspot Area");
 }
 void loop()
