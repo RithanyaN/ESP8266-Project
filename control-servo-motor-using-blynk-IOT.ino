be01185bcee4c21e00762eabd13fab5f57f135e3
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2023-12-29 21:39:29 +0530

    Create control-servo-motor-using-blynk-IOT.ino

---
+++ control-servo-motor-using-blynk-IOT.ino
@@ -0,0 +1,33 @@
+#define BLYNK_TEMPLATE_ID "TMPL3U5hsR682"
+#define BLYNK_TEMPLATE_NAME "Control Servo Motor"
+#define BLYNK_AUTH_TOKEN "s1DnjO7SVfrjN3DsnEsTlyzSJ43tFI7u"
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#include<Servo.h>
+Servo servo1;
+
+char auth[] = "s1DnjO7SVfrjN3DsnEsTlyzSJ43tFI7u";
+char ssid[] = "Device name";
+char pass[] = "Device password";
+
+BLYNK_WRITE(V0)
+{
+  int s0 = param.asInt(); 
+  servo1.write(s0);
+  Blynk.virtualWrite(V1, s0);
+} 
+
+void setup()
+{
+  Serial.begin(9600);
+  servo1.attach(D2); //Connect to Pin D2 in NodeMCU
+  Blynk.begin(auth, ssid, pass);//Splash screen delay
+  delay(2000); 
+}
+
+void loop() 
+{
+  Blynk.run();
+}
