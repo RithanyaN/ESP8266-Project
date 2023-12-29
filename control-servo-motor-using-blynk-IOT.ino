cd187de3961d5f57e259a420cba6a81640a0c93f
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2023-12-29 21:52:40 +0530

    Update control-servo-motor-using-blynk-IOT.ino

--- control-servo-motor-using-blynk-IOT.ino
+++ control-servo-motor-using-blynk-IOT.ino
@@ -1,3 +1,6 @@
+//In Blynk two web dashboards are used 1) Slider 2)Gauge(used to see the servo motors output i.e degree of rotation)
+//0-180 deg is used in both the dashboard
+//pins Virtual pins
 #define BLYNK_TEMPLATE_ID "TMPL3U5hsR682"
 #define BLYNK_TEMPLATE_NAME "Control Servo Motor"
 #define BLYNK_AUTH_TOKEN "s1DnjO7SVfrjN3DsnEsTlyzSJ43tFI7u"
